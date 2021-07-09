97b818faa698da9630ba75534342f26dd25370ac
root <root@DESKTOP-C57RD7B.localdomain>
2021-07-09 09:01:56 +0900

    aa

--- arraybinary.c
+++ arraybinary.c
@@ -1,29 +1,26 @@
 #include <stdio.h>
 
 int main()
+
+	int decimal = 13;
+	int binary[20] = {0,}
+	int position = 0;
+	while (1)
 {
-	    int decimal = 13;
-	        int binary[20] = { 0, };
-		    int position = 0;
+	binary[position] = deicmal % 2; //1, putting the result of 'decimal % 2' into the first position of the variable 'binary'.
+	decimal = decimal / 2; //6
+	position++; //position 2
 
-		        while (1)
-				    {
-					            binary[position] = decimal % 2;
-						            decimal = decimal / 2;
-							            position++;
-
-								            if (decimal == 0)
-										                break;
-									        }
-
-			    for (int i = position - 1; i >= 0; i--)
-				        {
-						        printf("%d", binary[i]);
-							    }
-
-			        printf("\n");
-
-				    return 0;
-
+	if (decimal == 0)
+		break;
 }
-~
+
+for (int i = position - 1; i>=0; i--)
+	{
+		printf("%d", binary[i]);
+	}
+
+printf("\n");
+
+return 0;
+}
